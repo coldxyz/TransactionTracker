5b0d20fd1ada07900024093914de33e158267704
Rúben Fonseca <rubenfonseca914@gmail.com>
2026-05-07 08:46:04 +0200

    feat: add nav to edit, add category closes #9

--- app/src/main/java/com/cold/transactiontracker/core/navigation/AppNavHost.kt
+++ app/src/main/java/com/cold/transactiontracker/core/navigation/AppNavHost.kt
@@ -12,13 +12,15 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.navigation
 import androidx.navigation.navArgument
+import com.cold.transactiontracker.features.categories.ui.AddCategoryDestination
 import com.cold.transactiontracker.features.categories.ui.CategoriesDestination
 import com.cold.transactiontracker.features.categories.ui.CategoriesListScreen
-import com.cold.transactiontracker.features.categories.ui.CategoryEntryDestination
 import com.cold.transactiontracker.features.categories.ui.CategoryEntryScreen
+import com.cold.transactiontracker.features.categories.ui.CategoryFlowDestination
 import com.cold.transactiontracker.features.categories.ui.CategorySelectionDestination
 import com.cold.transactiontracker.features.categories.ui.CategorySelectionScreen
 import com.cold.transactiontracker.features.categories.ui.CategoryViewModel
+import com.cold.transactiontracker.features.categories.ui.EditCategoryDestination
 import com.cold.transactiontracker.features.homescreen.ui.HomeDestination
 import com.cold.transactiontracker.features.homescreen.ui.HomeScreen
 import com.cold.transactiontracker.features.settings.ui.SettingsDestination
@@ -36,68 +38,217 @@ import com.cold.transactiontracker.features.transactions.ui.TransactionViewModel
 import com.cold.transactiontracker.features.transactions.ui.TransactionsListDestination
 import com.cold.transactiontracker.features.transactions.ui.TransactionsListScreen
 
+private fun NavHostController.navigateBackOrHome() {
+
+    val popped = popBackStack()
+
+    if (!popped) {
+
+        navigate(HomeDestination.route) {
+
+            popUpTo(graph.startDestinationId) {
+                inclusive = true
+            }
+
+            launchSingleTop = true
+        }
+    }
+}
+
 @Composable
 fun AppNavHost(
     navController: NavHostController,
     modifier: Modifier = Modifier
 ) {
+
     val currentRoute =
-        navController.currentBackStackEntryAsState().value?.destination?.route
+        navController.currentBackStackEntryAsState()
+            .value
+            ?.destination
+            ?.route
 
     NavHost(
         navController = navController,
         startDestination = HomeDestination.route,
         modifier = modifier
     ) {
+
+        // ---------------- HOME ----------------
+
         composable(HomeDestination.route) {
+
             HomeScreen(
                 currentRoute = currentRoute,
+
                 onNavigate = { route ->
+
                     navController.navigate(route) {
-                        popUpTo(navController.graph.startDestinationId) {
+
+                        popUpTo(
+                            navController.graph.startDestinationId
+                        ) {
                             saveState = true
                         }
+
                         launchSingleTop = true
+
                         restoreState = true
                     }
                 },
+
                 navigateToTransactionEntry = {
-                    navController.navigate(AddTransactionDestination.route)
+
+                    navController.navigate(
+                        TransactionFlowDestination.route
+                    )
                 },
+
                 navigateToSettings = {
-                    navController.navigate(SettingsDestination.route)
+
+                    navController.navigate(
+                        SettingsDestination.route
+                    )
                 }
             )
         }
 
+        // ---------------- SETTINGS ----------------
+
         composable(SettingsDestination.route) {
+
             SettingsScreen(
+
                 navigateBack = {
-                    navController.popBackStack()
+                    navController.navigateBackOrHome()
                 },
+
                 navigateToCategories = {
-                    navController.navigate(CategoriesDestination.route)
+
+                    navController.navigate(
+                        CategoriesDestination.route
+                    )
                 },
+
                 navigateToRecurring = {
                     // TODO
                 }
             )
         }
 
+        // ---------------- CATEGORIES LIST ----------------
+
         composable(CategoriesDestination.route) {
+
             CategoriesListScreen(
-                navigateBack = { navController.popBackStack() },
+
+                navigateBack = {
+                    navController.navigateBackOrHome()
+                },
+
                 navigateToAddCategory = {
-                    navController.navigate(CategoryEntryDestination.route)
+
+                    navController.navigate(
+                        CategoryFlowDestination.route
+                    )
+                },
+
+                navigateToEditCategory = { categoryId ->
+
+                    navController.navigate(
+                        EditCategoryDestination.createRoute(
+                            categoryId
+                        )
+                    )
                 }
             )
         }
 
-        composable(CategoryEntryDestination.route) {
-            CategoryEntryScreen(
-                navigateBack = { navController.popBackStack() }
-            )
+        // ---------------- CATEGORY FLOW ----------------
+
+        navigation(
+            startDestination = "category_root",
+            route = CategoryFlowDestination.route
+        ) {
+
+            composable("category_root") {
+
+                LaunchedEffect(Unit) {
+
+                    navController.navigate(
+                        AddCategoryDestination.route
+                    ) {
+
+                        popUpTo("category_root") {
+                            inclusive = true
+                        }
+                    }
+                }
+            }
+
+            composable(
+                route = AddCategoryDestination.route
+            ) { backStackEntry ->
+
+                val parentEntry = remember(backStackEntry) {
+
+                    navController.getBackStackEntry(
+                        CategoryFlowDestination.route
+                    )
+                }
+
+                val categoryViewModel: CategoryViewModel =
+                    hiltViewModel(parentEntry)
+
+                CategoryEntryScreen(
+                    viewModel = categoryViewModel,
+
+                    navigateBack = {
+                        navController.navigateBackOrHome()
+                    }
+                )
+            }
+
+            composable(
+                route = EditCategoryDestination.route,
+
+                arguments = listOf(
+                    navArgument(
+                        EditCategoryDestination.categoryIdArg
+                    ) {
+                        type = NavType.IntType
+                    }
+                )
+            ) { backStackEntry ->
+
+                val parentEntry = remember(backStackEntry) {
+
+                    navController.getBackStackEntry(
+                        CategoryFlowDestination.route
+                    )
+                }
+
+                val categoryViewModel: CategoryViewModel =
+                    hiltViewModel(parentEntry)
+
+                val categoryId =
+                    backStackEntry.arguments!!
+                        .getInt(
+                            EditCategoryDestination.categoryIdArg
+                        )
+
+                CategoryEntryScreen(
+                    viewModel = categoryViewModel,
+
+                    categoryId = categoryId,
+
+                    navigateBack = {
+                        navController.navigateBackOrHome()
+                    }
+                )
+            }
         }
+
+        // ---------------- TRANSACTIONS LIST ----------------
 
         composable(TransactionsListDestination.route) {
 
@@ -108,7 +259,7 @@ fun AppNavHost(
                 viewModel = transactionViewModel,
 
                 navigateBack = {
-                    navController.popBackStack()
+                    navController.navigateBackOrHome()
                 },
 
                 navigateToEditTransaction = { transactionId ->
@@ -122,65 +273,95 @@ fun AppNavHost(
             )
         }
 
+        // ---------------- FILTERED TRANSACTIONS ----------------
+
         composable(
-            route = "${FilteredTransactionsDestination.route}/{${FilteredTransactionsDestination.TYPE_ARG}}"
+            route =
+                "${FilteredTransactionsDestination.route}/" +
+                        "{${FilteredTransactionsDestination.TYPE_ARG}}"
         ) { backStackEntry ->
 
-            val typeString = backStackEntry.arguments?.getString(
-                FilteredTransactionsDestination.TYPE_ARG
-            ) ?: TransactionType.EXPENSE.name
+            val typeString =
+                backStackEntry.arguments?.getString(
+                    FilteredTransactionsDestination.TYPE_ARG
+                ) ?: TransactionType.EXPENSE.name
 
-            val type = TransactionType.valueOf(typeString)
+            val type =
+                TransactionType.valueOf(typeString)
 
-            val viewModel: TransactionViewModel = hiltViewModel()
+            val viewModel: TransactionViewModel =
+                hiltViewModel()
 
             FilteredTransactionsScreen(
                 type = type,
+
                 viewModel = viewModel,
-                navigateBack = { navController.popBackStack() },
-                navigateToEditTransaction = { transactionId ->
 
-                    navController.navigate(
-                        EditTransactionDestination.createRoute(
-                            transactionId
-                        )
-                    )
-                },
-            )
-        }
-
-        composable(
-            route = "${CategoryTransactionsDestination.route}/{${CategoryTransactionsDestination.CATEGORY_ID_ARG}}/{${CategoryTransactionsDestination.CATEGORY_NAME_ARG}}"
-        ) { backStackEntry ->
-
-            val categoryId = backStackEntry.arguments
-                ?.getString(CategoryTransactionsDestination.CATEGORY_ID_ARG)
-                ?.toIntOrNull()
-                ?: return@composable
-
-            val categoryName = backStackEntry.arguments
-                ?.getString(CategoryTransactionsDestination.CATEGORY_NAME_ARG)
-                ?: "Category"
-
-            val viewModel: TransactionViewModel = hiltViewModel()
-
-            CategoryTransactionsScreen(
-                categoryId = categoryId,
-                categoryName = categoryName,
-                viewModel = viewModel,
-                navigateToEditTransaction = { transactionId ->
-
-                    navController.navigate(
-                        EditTransactionDestination.createRoute(
-                            transactionId
-                        )
-                    )
-                },
                 navigateBack = {
-                    navController.popBackStack()
+                    navController.navigateBackOrHome()
+                },
+
+                navigateToEditTransaction = { transactionId ->
+
+                    navController.navigate(
+                        EditTransactionDestination.createRoute(
+                            transactionId
+                        )
+                    )
                 }
             )
         }
+
+        // ---------------- CATEGORY TRANSACTIONS ----------------
+
+        composable(
+            route =
+                "${CategoryTransactionsDestination.route}/" +
+                        "{${CategoryTransactionsDestination.CATEGORY_ID_ARG}}/" +
+                        "{${CategoryTransactionsDestination.CATEGORY_NAME_ARG}}"
+        ) { backStackEntry ->
+
+            val categoryId =
+                backStackEntry.arguments
+                    ?.getString(
+                        CategoryTransactionsDestination.CATEGORY_ID_ARG
+                    )
+                    ?.toIntOrNull()
+                    ?: return@composable
+
+            val categoryName =
+                backStackEntry.arguments
+                    ?.getString(
+                        CategoryTransactionsDestination.CATEGORY_NAME_ARG
+                    )
+                    ?: "Category"
+
+            val viewModel: TransactionViewModel =
+                hiltViewModel()
+
+            CategoryTransactionsScreen(
+                categoryId = categoryId,
+
+                categoryName = categoryName,
+
+                viewModel = viewModel,
+
+                navigateToEditTransaction = { transactionId ->
+
+                    navController.navigate(
+                        EditTransactionDestination.createRoute(
+                            transactionId
+                        )
+                    )
+                },
+
+                navigateBack = {
+                    navController.navigateBackOrHome()
+                }
+            )
+        }
+
+        // ---------------- TRANSACTION FLOW ----------------
 
         navigation(
             startDestination = "transaction_root",
@@ -194,6 +375,7 @@ fun AppNavHost(
                     navController.navigate(
                         AddTransactionDestination.route
                     ) {
+
                         popUpTo("transaction_root") {
                             inclusive = true
                         }
@@ -219,7 +401,7 @@ fun AppNavHost(
                     viewModel = transactionViewModel,
 
                     navigateBack = {
-                        navController.popBackStack()
+                        navController.navigateBackOrHome()
                     },
 
                     navigateToCategorySelection = {
@@ -265,7 +447,7 @@ fun AppNavHost(
                     transactionId = transactionId,
 
                     navigateBack = {
-                        navController.popBackStack()
+                        navController.navigateBackOrHome()
                     },
 
                     navigateToCategorySelection = {
@@ -300,7 +482,7 @@ fun AppNavHost(
                     categoryViewModel = categoryViewModel,
 
                     navigateBack = {
-                        navController.popBackStack()
+                        navController.navigateBackOrHome()
                     }
                 )
             }
